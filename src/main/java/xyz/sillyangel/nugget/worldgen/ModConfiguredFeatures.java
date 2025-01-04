ab0a3a26415ea047e084fb727b7d26aee526859f
sillyangel <sillyangel3@gmail.com>
2025-01-04 16:43:11 -0600

    ore gen

--- src/main/java/xyz/sillyangel/nugget/worldgen/ModConfiguredFeatures.java
+++ src/main/java/xyz/sillyangel/nugget/worldgen/ModConfiguredFeatures.java
@@ -1,5 +1,9 @@
 package xyz.sillyangel.nugget.worldgen;
 
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
+import net.minecraft.world.level.levelgen.structure.templatesystem.TagMatchTest;
 import xyz.sillyangel.nugget.NuggetMod;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
@@ -8,11 +12,24 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
+import xyz.sillyangel.nugget.block.ModBlocks;
+
+import java.util.List;
 
 public class ModConfiguredFeatures {
 
-    public static void bootstrap(BootstrapContext<ConfiguredFeature<?, ?>> context) {
+    public static final ResourceKey<ConfiguredFeature<?, ?>> OVERWORLD_NUGGET_ORE = registerKey("nugget_ore");
 
+
+    public static void bootstrap(BootstrapContext<ConfiguredFeature<?, ?>> context) {
+        RuleTest stoneReplaceables = new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES);
+        RuleTest deepslateReplaceables = new TagMatchTest(BlockTags.DEEPSLATE_ORE_REPLACEABLES);
+
+        List<OreConfiguration.TargetBlockState> overworldNuggetOres = List.of(
+                OreConfiguration.target(stoneReplaceables, ModBlocks.NUGGET_ORE.get().defaultBlockState()),
+                OreConfiguration.target(deepslateReplaceables, ModBlocks.NUGGET_DEEPSLATE_ORE.get().defaultBlockState()));
+
+        register(context, OVERWORLD_NUGGET_ORE, Feature.ORE, new OreConfiguration(overworldNuggetOres, 9));
 
     }
 
