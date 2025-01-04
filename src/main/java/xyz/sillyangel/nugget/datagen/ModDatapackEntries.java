24bb7a6438db934db2013c83c87e56d42c3a1ce5
sillyangel <sillyangel3@gmail.com>
2025-01-04 16:12:00 -0600

    world gen

--- src/main/java/xyz/sillyangel/nugget/datagen/ModDatapackEntries.java
+++ src/main/java/xyz/sillyangel/nugget/datagen/ModDatapackEntries.java
@@ -1,14 +1,17 @@
 package xyz.sillyangel.nugget.datagen;
 
 import xyz.sillyangel.nugget.NuggetMod;
-import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
 import xyz.sillyangel.nugget.trim.ModTrimMaterials;
 import xyz.sillyangel.nugget.trim.ModTrimPatterns;
+import xyz.sillyangel.nugget.worldgen.ModBiomeModifiers;
+import xyz.sillyangel.nugget.worldgen.ModConfiguredFeatures;
+import xyz.sillyangel.nugget.worldgen.ModPlacedFeatures;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
@@ -16,7 +19,11 @@ import java.util.concurrent.CompletableFuture;
 public class ModDatapackEntries extends DatapackBuiltinEntriesProvider {
     public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
             .add(Registries.TRIM_MATERIAL, ModTrimMaterials::bootstrap)
-            .add(Registries.TRIM_PATTERN, ModTrimPatterns::bootstrap);
+            .add(Registries.TRIM_PATTERN, ModTrimPatterns::bootstrap)
+
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap)
+            .add(ForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap);
 
     public ModDatapackEntries(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, BUILDER, Set.of(NuggetMod.MOD_ID));
