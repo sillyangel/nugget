7e9dff2c649051c65b4b09a038bdb58751aa8686
sillyangel <sillyangel3@gmail.com>
2025-01-06 15:54:21 -0600

    Nugget Tools are faster (as diamond) 4.5f to 8f

--- src/main/java/xyz/sillyangel/nugget/item/ModToolTiers.java
+++ src/main/java/xyz/sillyangel/nugget/item/ModToolTiers.java
@@ -6,7 +6,7 @@ import net.minecraftforge.common.ForgeTier;
 import xyz.sillyangel.nugget.util.ModTags;
 
 public class ModToolTiers {
-    public static final Tier NUGGET =  new ForgeTier(1500, 4.5f, 3.5f, 22,
+    public static final Tier NUGGET =  new ForgeTier(1500, 8f, 3.5f, 22,
             ModTags.Blocks.NEEDS_NUGGET_TOOL, () -> Ingredient.of(ModItems.NUGGET.get()),
             ModTags.Blocks.INCORRECT_FOR_NUGGET_TOOL);
 }
