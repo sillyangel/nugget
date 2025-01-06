7602fb947c22e931f3dfd1bdabaf8090b7058927
sillyangel <sillyangel3@gmail.com>
2025-01-06 15:55:46 -0600

    Nugget Tools are faster (as diamond) 4.5f to 8f

--- src/main/java/xyz/sillyangel/nugget/item/ModToolTiers.java
+++ src/main/java/xyz/sillyangel/nugget/item/ModToolTiers.java
@@ -5,5 +5,5 @@ import xyz.sillyangel.nugget.util.ModTags;
 
 public class ModToolTiers {
     public static final ToolMaterial NUGGET =  new ToolMaterial(ModTags.Blocks.INCORRECT_FOR_NUGGET_TOOL,
-            1500, 4.5f, 3.5f, 22, ModTags.Items.NUGGET_REPAIRS);
+            1500, 8f, 3.5f, 22, ModTags.Items.NUGGET_REPAIRS);
 }
