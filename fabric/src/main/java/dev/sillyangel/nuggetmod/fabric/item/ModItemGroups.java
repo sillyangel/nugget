fc81b81683c0eaefdbf259b3e4d502d3545cc1cf
angel <angel@sillyangel.dev>
2026-01-23 15:15:42 -0600

    fix again

--- fabric/src/main/java/dev/sillyangel/nuggetmod/fabric/item/ModItemGroups.java
+++ fabric/src/main/java/dev/sillyangel/nuggetmod/fabric/item/ModItemGroups.java
@@ -4,53 +4,55 @@ import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import dev.sillyangel.nuggetmod.block.ModBlocks;
 import dev.sillyangel.nuggetmod.item.ModItems;
 import dev.sillyangel.nuggetmod.fabric.NuggetMod;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.item.CreativeModeTab;
 
 public class ModItemGroups {
 
-    public static final ItemGroup NUGGET_BLOCKS_GROUP = Registry.register(Registries.ITEM_GROUP,
-            Identifier.of(NuggetMod.MOD_ID, "nugget_blocks"),
+    public static final CreativeModeTabs NUGGET_BLOCKS_GROUP = Registry.register(Registries.CREATIVE_MODE_TAB,
+            Identifier.fromNamespaceAndPath(NuggetMod.MOD_ID, "nugget_blocks"),
             FabricItemGroup.builder().icon(() -> new ItemStack(ModBlocks.NUGGET_BLOCK.get()))
-                    .displayName(Text.translatable("creativetab.nuggetmod.nugget_blocks"))
-                    .entries((displayContext, entries) -> {
-                        entries.add(ModBlocks.NUGGET_BLOCK.get());
-                        entries.add(ModBlocks.RAW_NUGGET_BLOCK.get());
-                        entries.add(ModBlocks.NUGGET_ORE.get());
-                        entries.add(ModBlocks.NUGGET_DEEPSLATE_ORE.get());
+                    .title(Component.translatable("creativetab.nuggetmod.nugget_blocks"))
+                    .displayItems((displayContext, entries) -> {
+                        entries.accept(ModBlocks.NUGGET_BLOCK.get());
+                        entries.accept(ModBlocks.RAW_NUGGET_BLOCK.get());
+                        entries.accept(ModBlocks.NUGGET_ORE.get());
+                        entries.accept(ModBlocks.NUGGET_DEEPSLATE_ORE.get());
                     }).build());
 
-    public static final ItemGroup NUGGET_ITEMS_GROUP = Registry.register(Registries.ITEM_GROUP,
-            Identifier.of(NuggetMod.MOD_ID, "nugget_items"),
+    public static final CreativeModeTab NUGGET_ITEMS_GROUP = Registry.register(Registries.CREATIVE_MODE_TAB,
+            Identifier.fromNamespaceAndPath(NuggetMod.MOD_ID, "nugget_items"),
             FabricItemGroup.builder().icon(() -> new ItemStack(ModItems.NUGGET.get()))
-                    .displayName(Text.translatable("creativetab.nuggetmod.nugget_items"))
-                    .entries((displayContext, entries) -> {
-                        entries.add(ModItems.NUGGET.get());
-                        entries.add(ModItems.RAW_NUGGET.get());
+                    .title(Component.translatable("creativetab.nuggetmod.nugget_items"))
+                    .displayItems((displayContext, entries) -> {
+                        entries.accept(ModItems.NUGGET.get());
+                        entries.accept(ModItems.RAW_NUGGET.get());
 
                         // Tool Set
-                        entries.add(ModItems.NUGGET_SWORD.get());
-                        entries.add(ModItems.NUGGET_PICKAXE.get());
-                        entries.add(ModItems.NUGGET_AXE.get());
-                        entries.add(ModItems.NUGGET_SHOVEL.get());
-                        entries.add(ModItems.NUGGET_HOE.get());
-                        entries.add(ModItems.NUGGET_SPEAR.get());
+                        entries.accept(ModItems.NUGGET_SWORD.get());
+                        entries.accept(ModItems.NUGGET_PICKAXE.get());
+                        entries.accept(ModItems.NUGGET_AXE.get());
+                        entries.accept(ModItems.NUGGET_SHOVEL.get());
+                        entries.accept(ModItems.NUGGET_HOE.get());
+                        entries.accept(ModItems.NUGGET_SPEAR.get());
 
                         // Armor
-                        entries.add(ModItems.NUGGET_HELMET.get());
-                        entries.add(ModItems.NUGGET_CHESTPLATE.get());
-                        entries.add(ModItems.NUGGET_LEGGINGS.get());
-                        entries.add(ModItems.NUGGET_BOOTS.get());
+                        entries.accept(ModItems.NUGGET_HELMET.get());
+                        entries.accept(ModItems.NUGGET_CHESTPLATE.get());
+                        entries.accept(ModItems.NUGGET_LEGGINGS.get());
+                        entries.accept(ModItems.NUGGET_BOOTS.get());
 
-                        entries.add(ModItems.NUGGET_HORSE_ARMOR.get());
+                        entries.accept(ModItems.NUGGET_HORSE_ARMOR.get());
 
-                        entries.add(ModItems.NUGGET_SMITHING_TEMPLATE.get());
+                        entries.accept(ModItems.NUGGET_SMITHING_TEMPLATE.get());
 
-                        entries.add(ModItems.NUGGET_MUSIC_DISC.get());
+                        entries.accept(ModItems.NUGGET_MUSIC_DISC.get());
                     }).build());
 
     public static void registerItemGroups() {
