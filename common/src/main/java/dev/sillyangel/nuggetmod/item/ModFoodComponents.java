115704fe79d5b9b18e463f2cda1cf91044b6a576
angel <angel@sillyangel.dev>
2026-03-24 12:35:05 -0500

    Update common/src/main/java/dev/sillyangel/nuggetmod/item/ModFoodComponents.java

--- common/src/main/java/dev/sillyangel/nuggetmod/item/ModFoodComponents.java
+++ common/src/main/java/dev/sillyangel/nuggetmod/item/ModFoodComponents.java
@@ -7,5 +7,8 @@ public class ModFoodComponents {
             .nutrition(5)
             .saturationModifier(0.5f)
             .build();
+    public static final FoodProperties NUGGET_CAKE = new FoodProperties.Builder()
+            .nutrition(3)
+            .saturationModifier(0.7f)
+            .build();
 }
-
