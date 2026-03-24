6396c55ae55841b63109a84076ac7b718438f766
angel <angel@sillyangel.dev>
2026-03-24 12:33:31 -0500

    Update common/src/main/java/dev/sillyangel/nuggetmod/item/ModItemGroups.java

--- common/src/main/java/dev/sillyangel/nuggetmod/item/ModItemGroups.java
+++ common/src/main/java/dev/sillyangel/nuggetmod/item/ModItemGroups.java
@@ -53,6 +53,8 @@ public class ModItemGroups {
                         entries.accept(ModItems.NUGGET_HORSE_ARMOR.get());
                         entries.accept(ModItems.NUGGET_SMITHING_TEMPLATE.get());
                         entries.accept(ModItems.NUGGET_MUSIC_DISC.get());
+
+                        entries.accept(ModItems.NUGGET_CAKE.get());
                     }).build());
 
     public static void init() {
